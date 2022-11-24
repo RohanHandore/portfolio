110c1e1d90d67b7523a1dd708e9d97b386098aa1
RohanHandore <110839432+RohanHandore@users.noreply.github.com>
2022-11-24 08:55:00 +0530

    map commit

--- assets/js/rohan.js
+++ assets/js/rohan.js
@@ -60,7 +60,7 @@ $(window).on("load", function() {
 function initMap() {
 // Styles a map in night mode.
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.674, lng: -73.945},
+        center: {lat: 19.997, lng: 73.789},
         zoom: 12,
         scrollwheel:  false,
         navigationControl: false,
